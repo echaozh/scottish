03cfc74d0fb3c6c98c8c83aec8459342f865632d
Zhang Yichao <echaozh@gmail.com>
2014-03-18 10:48:28 +0800

    catch sync exceptions from sql and convert to status 500

--- src/Web/Scottish/Database/Persist.hs
+++ src/Web/Scottish/Database/Persist.hs
@@ -4,14 +4,24 @@
 -}
 module Web.Scottish.Database.Persist where
 
+import Control.Exception.Enclosed
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
 
+import Data.Functor
+
 import Database.Persist.Sql
+
+import Network.HTTP.Types.Status
 
 import Web.Scottish
 import Web.Scottish.Database
 
 runSql :: (MonadTrans t, HasDatabaseConnectionPool Connection config)
-         => SqlPersistM a -> t (Scottish config s s') a
+       => SqlPersistM a -> t (Scottish config s s') a
 runSql s = liftIO . runSqlPersistMPool s >$< getPool
+
+runSql' :: (HasDatabaseConnectionPool Connection config)
+        => SqlPersistM a -> ScottishActionM' config s' a
+runSql' s = (runSql $ (Just <$> s) `catchAny` \_ -> return Nothing)
+            >>= maybe (raise internalServerError500) return
