63772bc3bce5d4736fba99f392546a26ac3c4d68
Zhang Yichao <echaozh@gmail.com>
2014-03-18 10:55:24 +0800

    more docs

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 Provides global read-only configuration and global/action-local read-write
 states to [Scotty](https://github.com/scotty-web/scotty).
+
+For now, `scottish` also provides helpers for database access, especially with
+persistent.
--- src/Web/Scottish/Database/Persist.hs
+++ src/Web/Scottish/Database/Persist.hs
@@ -21,6 +21,8 @@ runSql :: (MonadTrans t, HasDatabaseConnectionPool Connection config)
        => SqlPersistM a -> t (Scottish config s s') a
 runSql s = liftIO . runSqlPersistMPool s >$< getPool
 
+-- | Catches any synchronous exceptions thrown when executing the database
+-- transaction, and 'raise' an 'internalServerError500' instead.
 runSql' :: (HasDatabaseConnectionPool Connection config)
         => SqlPersistM a -> ScottishActionM' config s' a
 runSql' s = (runSql $ (Just <$> s) `catchAny` \_ -> return Nothing)
